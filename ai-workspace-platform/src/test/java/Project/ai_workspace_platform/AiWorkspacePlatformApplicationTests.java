8fd8fdcd90482dd0dced4cf568c80879600b991c
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-27 22:16:05 +0530

    Initial commit

---
+++ ai-workspace-platform/src/test/java/Project/ai_workspace_platform/AiWorkspacePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.ai_workspace_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWorkspacePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
