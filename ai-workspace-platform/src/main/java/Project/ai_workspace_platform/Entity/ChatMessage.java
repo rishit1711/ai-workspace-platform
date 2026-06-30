38762e3b969d44f501350f0fe003312c86002494
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-30 23:29:29 +0530

    Started with Entity Development..

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/Entity/ChatMessage.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/Entity/ChatMessage.java
@@ -1,7 +1,14 @@
 package Project.ai_workspace_platform.Entity;
 
-import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
+import java.time.Instant;
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatMessage {
     Long id;
     ChatSession chatSession;
