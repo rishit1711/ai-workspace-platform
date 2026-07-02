e05e5d337b02140b10c05848e98463c99c05d4d4
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-02 22:42:02 +0530

    Started with REST API Development.

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/service/UserService.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/service/UserService.java
@@ -1,7 +1,7 @@
 package Project.ai_workspace_platform.service;
 
 import Project.ai_workspace_platform.dto.Auth.UserProfileResponse;
-import org.jspecify.annotations.Nullable;
+
 
 public interface UserService {
      UserProfileResponse getProfile(Long id);
