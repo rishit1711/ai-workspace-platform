f3073ede0b1af58a9f44b95ca1089380c751a8b5
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-05 13:28:29 +0530

    Service Implementation

---
+++ target/generated-sources/annotations/Project/ai_workspace_platform/mapper/ProjectMapperImpl.java
@@ -0,0 +1,61 @@
+package Project.ai_workspace_platform.mapper;
+
+import Project.ai_workspace_platform.dto.Auth.UserProfileResponse;
+import Project.ai_workspace_platform.dto.Project.ProjectResponse;
+import Project.ai_workspace_platform.entity.Project;
+import Project.ai_workspace_platform.entity.User;
+import java.time.Instant;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-07-05T12:02:14+0530",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
+)
+@Component
+public class ProjectMapperImpl implements ProjectMapper {
+
+    @Override
+    public ProjectResponse toProjectResponse(Project project) {
+        if ( project == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String name = null;
+        Instant createdAt = null;
+        Instant updatedAt = null;
+        UserProfileResponse owner = null;
+
+        id = project.getId();
+        name = project.getName();
+        createdAt = project.getCreatedAt();
+        updatedAt = project.getUpdatedAt();
+        owner = userToUserProfileResponse( project.getOwner() );
+
+        ProjectResponse projectResponse = new ProjectResponse( id, name, createdAt, updatedAt, owner );
+
+        return projectResponse;
+    }
+
+    protected UserProfileResponse userToUserProfileResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String email = null;
+        String name = null;
+        String avatarUrl = null;
+
+        id = user.getId();
+        email = user.getEmail();
+        name = user.getName();
+        avatarUrl = user.getAvatarUrl();
+
+        UserProfileResponse userProfileResponse = new UserProfileResponse( id, email, name, avatarUrl );
+
+        return userProfileResponse;
+    }
+}
