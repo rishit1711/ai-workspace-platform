ccd11fd0cfac100107b3ad166e0615b4f4dba102
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-09 19:12:20 +0530

    Service Implementation

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/Repository/ProjectMemberRepository.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/Repository/ProjectMemberRepository.java
@@ -3,7 +3,13 @@ package Project.ai_workspace_platform.Repository;
 import Project.ai_workspace_platform.entity.ProjectMember;
 import Project.ai_workspace_platform.entity.ProjectMemberId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
+
+
+    List<ProjectMember> findByIdProjectId(Long projectId);
 
 }
--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/service/impl/ProjectMemberServiceImpl.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/service/impl/ProjectMemberServiceImpl.java
@@ -1,23 +1,47 @@
 package Project.ai_workspace_platform.service.impl;
 
 import Project.ai_workspace_platform.Repository.ProjectMemberRepository;
+import Project.ai_workspace_platform.Repository.ProjectRepository;
 import Project.ai_workspace_platform.dto.member.MemberRequest;
 import Project.ai_workspace_platform.dto.member.MemberResponse;
 import Project.ai_workspace_platform.dto.member.UpdateRoleRequest;
+import Project.ai_workspace_platform.entity.Project;
+import Project.ai_workspace_platform.entity.ProjectMember;
+import Project.ai_workspace_platform.entity.User;
+import Project.ai_workspace_platform.mapper.ProjectMemberMapper;
 import Project.ai_workspace_platform.service.ProjectMemberService;
+import Project.ai_workspace_platform.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectMemberServiceImpl implements ProjectMemberService {
 
     private final ProjectMemberRepository projectMemberRepository;
+    private final ProjectRepository projectRepository;
+    private final ProjectMemberMapper projectMemberMapper;
     @Override
     public List<MemberResponse> getProjectMembers(Long projectId, Long userId) {
-        return List.of();
+        Project project = projectRepository.findAccessibleProjectById(projectId,userId).orElseThrow();
+        List<MemberResponse> memberResponseList = new ArrayList<>();
+        memberResponseList.add(projectMemberMapper.toProjectMemberResponse(project.getOwner()));
+        memberResponseList.addAll(
+                projectMemberRepository.findByIdProjectId(projectId)
+                        .stream()
+                        .map(projectMember -> projectMemberMapper.toProjectMemberResponseFromMember(projectMember))
+                        .collect(Collectors.toList()));
+
+
+
+
+
+
+        return memberResponseList;
     }
 
     @Override
