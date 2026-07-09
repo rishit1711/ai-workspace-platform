48e5f3350dee7d29fe58696e3d7899c1572e8d1b
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-09 17:52:54 +0530

    Service Implementation

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ProjectMember.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ProjectMember.java
@@ -3,6 +3,7 @@ package Project.ai_workspace_platform.entity;
 import Project.ai_workspace_platform.enums.ProjectRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 
@@ -17,12 +18,15 @@ public class ProjectMember {
     @EmbeddedId
     private ProjectMemberId id;
     @ManyToOne
-    @MapsId("projectId")
+    @MapsId("projectId")   // ek entity ki primary key dursi entity ki primary key se hi derive hoti hai
     private Project project;
     @ManyToOne
     @MapsId("userId")
     private User user;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private ProjectRole role;
+
     private Instant invitedAt;
     private Instant acceptedAt;
 
