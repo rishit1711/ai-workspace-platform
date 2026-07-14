80f55c53a7398115b27b16cfcff273ca2e302a63
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-14 23:34:18 +0530

    Indexing

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/Project.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/Project.java
@@ -13,20 +13,42 @@ import java.time.Instant;
 @NoArgsConstructor
 @Entity
 @Builder
+@Table(
+        indexes = {
+                @Index(
+                        name = "idx_project_owner_created",
+                        columnList = "owner_id, created_at"
+                ),
+                @Index(
+                        name = "idx_project_public",
+                        columnList = "is_public"
+                ),
+                @Index(
+                        name = "idx_project_deleted",
+                        columnList = "deleted_at"
+                )
+        }
+)
 public class Project {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     @ManyToOne
-    @JoinColumn(name = "owner_id",nullable = false)
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
-    private Boolean isPublic=false;
+
+    private Boolean isPublic = false;
+
     @CreationTimestamp
     private Instant createdAt;
+
     private Instant deletedAt;
+
     @UpdateTimestamp
     private Instant updatedAt;
-
 }
