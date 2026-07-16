47b005d151d67972f8b2aa949f0392a1b7d9d747
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-16 20:43:31 +0530

    Authentication Impl

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/controller/ProjectController.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/controller/ProjectController.java
@@ -3,11 +3,13 @@ package Project.ai_workspace_platform.controller;
 import Project.ai_workspace_platform.dto.Project.ProjectRequest;
 import Project.ai_workspace_platform.dto.Project.ProjectResponse;
 import Project.ai_workspace_platform.dto.Project.ProjectSummaryResponse;
+import Project.ai_workspace_platform.entity.User;
 import Project.ai_workspace_platform.service.ProjectService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,35 +23,39 @@ public class ProjectController {
 
 
     @GetMapping
-    public ResponseEntity<List<ProjectSummaryResponse>> getprojects(){
-        Long userId = 1L;   // later spring security se current logged-in user mil jayega
-        return ResponseEntity.ok(projectService.getMyProjects(userId));
+    public ResponseEntity<List<ProjectSummaryResponse>> getProjects(){
+        User user = getUser();
+        return ResponseEntity.ok(projectService.getMyProjects(user.getId()));
     }
     @GetMapping("/{id}")
     public ResponseEntity<ProjectResponse> getProject(@PathVariable Long id){
-        Long userId=1L;
-        return ResponseEntity.ok(projectService.getProjectById(id,userId));
+        User user = getUser();
+        return ResponseEntity.ok(projectService.getProjectById(id, user.getId()));
     }
 
     @PostMapping
     public ResponseEntity<ProjectResponse> createProject(@RequestBody @Valid ProjectRequest projectRequest){
 
-        Long userId=1L;
-        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(projectRequest,userId));
+        User user = getUser();
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(projectRequest, user.getId()));
 
     }
     @PatchMapping("/{id}")
     public ResponseEntity<ProjectResponse> updateProject(@PathVariable Long id,@RequestBody @Valid ProjectRequest projectRequest){
-        Long userId=1L;
-        return ResponseEntity.ok(projectService.updateProject(id,projectRequest,userId));
+        User user = getUser();
+        return ResponseEntity.ok(projectService.updateProject(id,projectRequest,user.getId()));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProject(@PathVariable Long id){
-        Long userId=1L;
-        projectService.softDelete(id,userId);
+        User user = getUser();
+        projectService.softDelete(id, user.getId());
         return ResponseEntity.noContent().build();
 
+    }
+
+    public User getUser(){
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 
