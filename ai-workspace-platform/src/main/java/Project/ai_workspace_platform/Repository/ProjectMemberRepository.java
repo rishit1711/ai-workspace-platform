cacc4fb61b3eadc91f0a0efa7173a14d19ee6b93
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-21 22:56:49 +0530

    Authorization Impl

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/Repository/ProjectMemberRepository.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/Repository/ProjectMemberRepository.java
@@ -2,14 +2,24 @@ package Project.ai_workspace_platform.Repository;
 
 import Project.ai_workspace_platform.entity.ProjectMember;
 import Project.ai_workspace_platform.entity.ProjectMemberId;
+import Project.ai_workspace_platform.enums.ProjectRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
 
 
     List<ProjectMember> findByIdProjectId(Long projectId);
-
+    @Query(
+            """
+                    SELECT pm.role FROM ProjectMember pm
+                    WHERE pm.id.projectId= :id
+                    AND pm.id.userId= :userId
+                    """
+    )
+    Optional<ProjectRole> findRoleByProjectIdAndUserId(@Param("projectId") Long id,@Param("userId") Long userId);
 }
