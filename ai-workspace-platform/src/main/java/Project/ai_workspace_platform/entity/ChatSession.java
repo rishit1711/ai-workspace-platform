0fd9bda19ed5f40ba555d9d53d83440d770ea2c4
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-28 19:36:17 +0530

    AI Module Started

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ChatSession.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ChatSession.java
@@ -1,21 +1,33 @@
 package Project.ai_workspace_platform.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatSession {
-
+    @EmbeddedId
+    private ChatSessionId chatSessionId;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "project_id",nullable = false)
+    @MapsId("projectId")
     Project project;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name="user_id",nullable = false)
+    @MapsId("userId")
     User user;
-    String  title;
+    @UpdateTimestamp
     Instant updatedAt;
+    @CreationTimestamp
+    @Column(nullable = false,unique = false)
     Instant createdAt;
     Instant deletedAt;
 }
