171fa90a76b4b93d39c0d442df3282d0bcffd03d
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-28 19:25:42 +0530

    AI Module Started

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ChatMessage.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ChatMessage.java
@@ -1,20 +1,28 @@
 package Project.ai_workspace_platform.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class ChatMessage {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
     ChatSession chatSession;
+    @Column(nullable = false)
     String content;
     String toolcals;
     Integer tokenUsed;
+    @CreationTimestamp
     Instant CreatedAt;
 
 }
