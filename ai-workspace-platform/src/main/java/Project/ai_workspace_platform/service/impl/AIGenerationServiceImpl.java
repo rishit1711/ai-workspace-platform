8af2300079d022bb0d16b3e37857539c3ac7642e
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-30 19:04:14 +0530

    AIGenerationServiceImpl

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/service/impl/AIGenerationServiceImpl.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/service/impl/AIGenerationServiceImpl.java
@@ -1,12 +1,22 @@
 package Project.ai_workspace_platform.service.impl;
 
+import Project.ai_workspace_platform.security.SecurityExpressions;
 import Project.ai_workspace_platform.service.AIGenerationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class AIGenerationServiceImpl implements AIGenerationService {
+    private final ChatClient chatClient;
+    private final SecurityExpressions expressions;
     @Override
-    public Flux<String> streamResponse(String message, Long aLong) {
+    @PreAuthorize("@security.canViewProject(#projectId)")
+    public Flux<String> streamResponse(String message, Long projectId) {
         return null;
     }
 }
