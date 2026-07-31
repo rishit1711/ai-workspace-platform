9e637c907af9d5569e5dc002f067990b53e37694
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-31 13:08:24 +0530

    working on file storage

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ProjectFile.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/entity/ProjectFile.java
@@ -1,15 +1,33 @@
 package Project.ai_workspace_platform.entity;
 
-import java.time.Instant;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.Instant;
+@Entity
 public class ProjectFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "projectId")
     private Project project;
+    @Column(nullable = false)
     private String path;
     private String minioObjectKey;
+    @CreationTimestamp
     private Instant createdAt;
+    @UpdateTimestamp
     private Instant updatedAt;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "created_by")
     private User createdBy;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "updated_by")
     private User updatedBy;
+    @Column(nullable = false)
+    private String fileName;
+
 
 }
