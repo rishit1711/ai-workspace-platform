e6a7b8f8d1fc5b8df2389b2281064b388915bd6b
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-04 19:22:36 +0530

    ProjectFileServiceImpl

---
+++ target/generated-sources/annotations/Project/ai_workspace_platform/mapper/ProjectFileMapperImpl.java
@@ -0,0 +1,45 @@
+package Project.ai_workspace_platform.mapper;
+
+import Project.ai_workspace_platform.dto.Files.FileNode;
+import Project.ai_workspace_platform.entity.ProjectFile;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-08-04T18:53:49+0530",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
+)
+@Component
+public class ProjectFileMapperImpl implements ProjectFileMapper {
+
+    @Override
+    public List<FileNode> toListOfFileNode(List<ProjectFile> projectFileList) {
+        if ( projectFileList == null ) {
+            return null;
+        }
+
+        List<FileNode> list = new ArrayList<FileNode>( projectFileList.size() );
+        for ( ProjectFile projectFile : projectFileList ) {
+            list.add( projectFileToFileNode( projectFile ) );
+        }
+
+        return list;
+    }
+
+    protected FileNode projectFileToFileNode(ProjectFile projectFile) {
+        if ( projectFile == null ) {
+            return null;
+        }
+
+        String path = null;
+
+        path = projectFile.getPath();
+
+        FileNode fileNode = new FileNode( path );
+
+        return fileNode;
+    }
+}
