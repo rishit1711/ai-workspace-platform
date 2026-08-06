0e4d915c574e498651b115c1889d2820da86bed5
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-06 18:51:47 +0530

    Adding Advisors

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/config/advisors/FileTreeContextAdvisor.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/config/advisors/FileTreeContextAdvisor.java
@@ -6,15 +6,26 @@ import org.springframework.ai.chat.client.ChatClientResponse;
 import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
 import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
 
+import java.util.Map;
+
 public class FileTreeContextAdvisor implements CallAdvisor {
     @Override
-    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
-        return null;
+    public ChatClientResponse adviseCall(ChatClientRequest request, CallAdvisorChain callAdvisorChain) {
+        Map<String,Object> context = request.context();
+        Long projectId= Long.parseLong((String) context.getOrDefault("projectId",0));
+
+        ChatClientRequest augumentedchatRequest = augumentRequestWithFileTree(request,projectId);
+
+        return callAdvisorChain.nextCall(augumentedchatRequest);
+    }
+
+    private ChatClientRequest augumentRequestWithFileTree(ChatClientRequest request,Long projectId){
+
     }
 
     @Override
     public String getName() {
-        return "";
+        return "FileTreeContextAdvisor";
     }
 
     @Override
