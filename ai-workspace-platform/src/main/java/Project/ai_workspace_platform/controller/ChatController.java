0de765dffadc412c59fe6ab3712f370525a8c888
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-12 19:42:09 +0530

    Chat Service Impl

--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/controller/ChatController.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/controller/ChatController.java
@@ -1,20 +1,23 @@
 package Project.ai_workspace_platform.controller;
 
 import Project.ai_workspace_platform.dto.chat.ChatRequest;
+import Project.ai_workspace_platform.dto.chat.ChatResponse;
 import Project.ai_workspace_platform.service.AIGenerationService;
+import Project.ai_workspace_platform.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
 
     private final AIGenerationService aiGenerationService;
+    private final ChatService chatService;
 
 
     @PostMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -27,4 +30,10 @@ public class ChatController {
                 .data((String) data)
                 .build());
     }
+
+    @GetMapping("/projects/{projectId}")
+    public List<ChatResponse> getChatHistory(@PathVariable Long projectId){
+        return chatService.getProjectChatHistory(projectId);
+    }
+
 }
--- ai-workspace-platform/src/main/java/Project/ai_workspace_platform/dto/chat/ChatResponse.java
+++ ai-workspace-platform/src/main/java/Project/ai_workspace_platform/dto/chat/ChatResponse.java
@@ -1,6 +1,26 @@
 package Project.ai_workspace_platform.dto.chat;
 
+import Project.ai_workspace_platform.entity.ChatEvent;
+import Project.ai_workspace_platform.enums.MessageRole;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.List;
+
 public record ChatResponse(
+
+        Long id,
+
+        String content,
+
+        List<ChatEvent> chatEventList,
+
+
+        MessageRole role,
+        Integer tokenUsed,
+
+         Instant CreatedAt
 
 ) {
 }
